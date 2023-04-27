9cdb8ecdacbf8271cdc11e54f1588de1e7ddaade
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-04-27 13:30:03 -0500

    App HTML structure and creative assets

--- client/src/components/AddProductPage.js
+++ client/src/components/AddProductPage.js
@@ -1,10 +1,65 @@
+// Imports - Dependencies
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AddProduct() {
+  // Defines component variables
+  const productName = "";
+  const sku = "";
+  const supplier = "";
+  const price = "";
+  const quantity = "";
+
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <p>This is the Add Product page.</p>
+      <h1>Add Product</h1>
+      {/* Add product form component */}
+      <form>
+        <div>
+          <label htmlFor="productName">Product Name:</label>
+          <input
+            type="text"
+            id="productName"
+            value={productName}
+          />
+        </div>
+        <div>
+          <label htmlFor="sku">SKU:</label>
+          <input
+            type="text"
+            id="sku"
+            value={sku}
+          />
+        </div>
+        <div>
+          <label htmlFor="supplier">Supplier:</label>
+          <input
+            type="text"
+            id="supplier"
+            value={supplier}
+          />
+        </div>
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            value={price}
+          />
+        </div>
+        <div>
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            type="number"
+            id="quantity"
+            value={quantity}
+          />
+        </div>
+        <Link to="/">
+          <button>Back</button>
+        </Link>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
--- client/src/components/UpdateProductPage.js
+++ client/src/components/UpdateProductPage.js
@@ -1,12 +1,67 @@
+// Imports - Dependencies
 import React from "react";
+import { Link } from "react-router-dom";
 
-function UpdateProduct() {
+function AddProduct() {
+  // Defines component variables
+  const productName = "";
+  const sku = "";
+  const supplier = "";
+  const price = "";
+  const quantity = "";
+
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <p>This is the Update Product page.</p>
+      <h1>Update Product</h1>
+      {/* Update product form component */}
+      <form>
+        <div>
+          <label htmlFor="productName">Product Name:</label>
+          <input
+            type="text"
+            id="productName"
+            value={productName}
+          />
+        </div>
+        <div>
+          <label htmlFor="sku">SKU:</label>
+          <input
+            type="text"
+            id="sku"
+            value={sku}
+          />
+        </div>
+        <div>
+          <label htmlFor="supplier">Supplier:</label>
+          <input
+            type="text"
+            id="supplier"
+            value={supplier}
+          />
+        </div>
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            value={price}
+          />
+        </div>
+        <div>
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            type="number"
+            id="quantity"
+            value={quantity}
+          />
+        </div>
+        <Link to="/">
+          <button>Back</button>
+        </Link>
+        <button type="submit">Update</button>
+      </form>
     </div>
   );
 }
 
-export default UpdateProduct;
+export default AddProduct;
