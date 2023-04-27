1b786fa5ba5d1282100437cea91d2cba71cbbb9f
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-04-27 12:15:00 -0500

    Front end file structure

---
+++ client/src/components/AddProductPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddProduct() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is the Add Product page.</p>
+    </div>
+  );
+}
+
+export default AddProduct;
